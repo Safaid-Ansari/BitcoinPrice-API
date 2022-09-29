5a039fa415e1d391f5954f8ac558b6acce6a4903
Safaid-Ansari <safaidansari07@gmail.com>
2022-09-29 12:26:33 +0530

    ..

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 
 // create default router it is baseurl
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome In Price Tracker </h1>");
-});
+
 app.use("/Bitcoin", router);
+app.get("/", (req, res) => {
+  res.send(
+    "<body style='background-color:cyan'><h1 style='text-align:center; font-size:50px'>Welcome to Bitcoin Price Tracker </h1></body>"
+  );
+});
+app.get("*", (req, res) => {
+  res.send(
+    "<body style='background-color:cyan'><h1 style='text-align:center; font-size:50px'>ERROR 404 </h1></body>"
+  );
+});
 
 app.listen(port, (error) => {
   if (error) console.log("ERROR", error);
